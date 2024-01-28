0467cade76ac27f17a8a2df1de191e1be881b84f
turbold <turbold@sodonsolution.com>
2024-01-28 21:23:31 +0800

    api

--- API.js
+++ API.js
@@ -1,280 +1,113 @@
+export const get = async function (url) {
+    try {
+        const response = await fetch(url, {
+            credentials: 'include',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            }
+        })
 
+        const status = response.status;
 
-export const get = function (url, callback, apiUrl) {
-    const API_URL = apiUrl;
-    let status = 500;
-    fetch(API_URL + url, {
-        credentials: 'include',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
+        if (response.ok) {
+            const jsonData = await response.json();
+            return {
+                success: true,
+                status,
+                payload: jsonData,
+            }
+        } else {
+            const jsonData = await response.json();
+            return {
+                success: false,
+                status,
+                message: jsonData.message || 'Unknown error'
+            }
         }
-    })
-        .then(function (response) {
-            status = response.status
-            if (response.ok) {
-                if (response.status === 204) { return null }
-                return response.json()
-            }
-            // throw new Error(response.status)
-        })
-        .then(function (jsonData) {
-            if (jsonData !== undefined) {
-                if (callback && typeof callback === 'function') {
-                    callback({
-                        success: true,
-                        status,
-                        payload: jsonData
-                    })
-                }
-            }
-        })
-        .catch((error) => {
-            if (callback && typeof callback === 'function') {
-                callback({
-                    success: false,
-                    status,
-                    message: isNaN(error.message)
-                        ? error.message
-                        : parseInt(error.message)
-                })
-            }
-        })
-}
-
-export const deleteRequest = function (url, callback, apiUrl) {
-    const API_URL = apiUrl;
-    let status = 500;
-    fetch(API_URL + url, {
-        credentials: 'same-origin',
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
+    } catch (e) {
+        console.warn("SODON-HTTP: Error while getting.", e);
+        return {
+            success: false,
+            status: 500,
+            message: e.message || "Unknown error",
         }
-    })
-        .then(function (response) {
-            status = response.status
-            if (response.ok) {
-                if (response.status === 204) {
-                    return null
-                }
-                return response.json()
-            }
-            throw new Error(response.status)
-        })
-        .then(function (jsonData) {
-            if (jsonData !== undefined) {
-                if (callback && typeof callback === 'function') {
-                    callback({
-                        success: true,
-                        status,
-                        payload: jsonData
-                    })
-                }
-            }
-        })
-        .catch((error) => {
-            if (callback && typeof callback === 'function') {
-                callback({
-                    success: false,
-                    status,
-                    message: isNaN(error.message)
-                        ? error.message
-                        : parseInt(error.message)
-                })
-            }
-        })
+    }
 }
 
-export const put = function (url, data, callback, apiUrl) {
-    const API_URL = apiUrl;
-    if (callback === undefined) {
-        callback = data
-        data = null
-    }
-    let status = 500
-    fetch(API_URL + url, {
-        credentials: 'same-origin',
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: data
-    })
-        .then(function (response) {
-            status = response.status
-            if (response.ok) {
-                if (response.status === 204) return null
-                return response.json()
-            }
-            throw new Error(response.status)
-        })
-        .then(function (jsonData) {
-            if (jsonData !== undefined) {
-                if (callback && typeof callback === 'function') {
-                    callback({
-                        success: true,
-                        payload: jsonData
-                    })
-                }
-            }
-        })
-        .catch((error) => {
-            if (callback && typeof callback === 'function') {
-                callback({
-                    success: false,
-                    status,
-                    message: isNaN(error.message)
-                        ? error.message
-                        : parseInt(error.message)
-                })
-            }
-        })
-}
+export const postJson = async function (url, data) {
+    try {
+        const response = await fetch(url, {
+            credentials: "include",
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
 
-export const postData = function (url, data, callback) {
-    let status = 500
-    if (callback === undefined) {
-        callback = data
-        data = null
-    }
+        const status = response.status;
 
-    fetch(`${API_URL}${url}`, {
-        credentials: 'include',
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then(function (response) {
-            status = response.status
-            if (response.ok) {
-                if (response.status === 204) {
-                    return null
-                }
-                return response.json()
-            } else {
-                return response.text()
+        if (response.ok) {
+            const jsonData = await response.json();
+            return {
+                success: true,
+                status,
+                payload: jsonData,
             }
-            // throw new Error(response);
-        })
-        .then(function (jsonData) {
-            if (jsonData !== undefined) {
-                callback({
-                    success: true,
-                    status,
-                    payload: jsonData
-                })
-            }
-        })
-        .catch((error) => {
-            callback({
+        } else {
+            const jsonData = await response.json();
+            return {
                 success: false,
                 status,
-                message: isNaN(error.message)
-                    ? error.message
-                    : parseInt(error.message)
-            })
-        })
+                message: jsonData.message || "Unknown error",
+            }
+        }
+    } catch (e) {
+        console.warn("SODON-HTTP: Error while post request.", e);
+        return {
+            success: false,
+            status: 500,
+            message: e.message || "Unknown error",
+        }
+    }
 }
 
-export const postForm = function (url, data, callback) {
-    if (callback === undefined) {
-        callback = data
-        data = null
-    }
-    let status = 500
+export const postForm = async function (url, form) {
+    try {
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+            },
+            body: form,
+            redirect: "follow"
+        });
 
-    const headers = new Headers()
-    headers.append('Accept', 'application/json')
-    // headers.append("Content-Type", "application/json");
+        const status = response.status;
 
-    fetch(`${API_URL}${url}`, {
-        method: 'POST',
-        headers,
-        body: data,
-        redirect: 'follow'
-    })
-        .then(function (response) {
-            status = response.status
-            if (response.ok) {
-                if (response.status === 204) {
-                    return null
-                }
-                return response.json()
-            } else {
-                return response.json()
+        if (response.ok) {
+            const jsonData = await response.json();
+            return {
+                success: true,
+                status,
+                payload: jsonData,
             }
-        })
-        .then(function (jsonData) {
-            if (jsonData !== undefined) {
-                callback({
-                    success: true,
-                    status,
-                    payload: jsonData
-                })
-            }
-        })
-        .catch((error) => {
-            console.warn('Error: ', error)
-            callback({
+        } else {
+            const jsonData = await response.json();
+            return {
                 success: false,
                 status,
-                message: isNaN(error.message)
-                    ? error.message
-                    : parseInt(error.message)
-            })
-            return { status }
-        })
-}
-
-export const postFormBrowser = function (url, data, callback) {
-    if (callback === undefined) {
-        callback = data
-        data = null
+                message: jsonData.message || "Unknown error",
+            }
+        }
+    } catch (e) {
+        console.warn("SODON-HTTP Error while posting form", e);
+        return {
+            success: false,
+            status: 500,
+            message: e.message || "Unknown error",
+        }
     }
-    let status = 500
-
-    const headers = new Headers()
-    headers.append('Accept', 'application/json')
-    // headers.append("Content-Type", "application/json");
-
-    fetch(`${API_URL}${url}`, {
-        method: 'POST',
-        headers,
-        body: data,
-        redirect: 'follow'
-    })
-        .then(function (response) {
-            status = response.status
-            if (response.ok) {
-                if (response.status === 204) {
-                    return null
-                }
-                return response.json()
-            } else {
-                return response.text()
-            }
-        })
-        .then(function (jsonData) {
-            if (jsonData !== undefined) {
-                callback({
-                    status,
-                    payload: jsonData
-                })
-            }
-        })
-        .catch((error) => {
-            callback({
-                status,
-                payload: error
-            })
-            return { status }
-            /* callback({
-                statusCode: status,
-                success: false,
-            }); */
-        })
 }
